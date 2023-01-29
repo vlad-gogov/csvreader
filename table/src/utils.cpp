2e8e43d2e4e82f249d92353327a8599c78f6f861
Vladislav Gogov <43129455+vlad-gogov@users.noreply.github.com>
2023-01-29 11:41:53 +0300

    fix all_of

--- table/src/utils.cpp
+++ table/src/utils.cpp
@@ -39,5 +39,5 @@ bool utils::isInteger(const std::string &str) {
     auto first = str.front();
     if (first != '-' && first != '+' && !std::isdigit(first))
         return false;
-    return std::all_of(std::next(str.begin()), str.end(), [](const char &ch) { std::isdigit(ch); });
+    return std::all_of(std::next(str.begin()), str.end(), [](const char &ch) { return std::isdigit(ch); });
 }
